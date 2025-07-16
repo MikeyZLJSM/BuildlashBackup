69eeba3cc8e81a212401188ff3882edb87326b01
lagdads <lagdads@foxmail.com>
2025-07-16 13:22:26 +0800

    修改ScriptManager为ModulesManager。添加注释

--- Assets/Scripts/Module/BaseModule.Battle.cs
+++ Assets/Scripts/Module/BaseModule.Battle.cs
@@ -5,16 +5,24 @@ namespace Module
 {
     public abstract partial class BaseModule
     {
-        // 溅射范围，仅在群体攻击时生效
+        /// <summary>溅射范围，仅在群体攻击时生效</summary>
         [NonSerialized] private int _splashRadius;
-        [NonSerialized] public int Attack; // 攻击力
-        [NonSerialized] public float AttackCooldown = 0f; // 攻击冷却时间
-        [NonSerialized] public float AttackSpeed = 1.0f; // 攻击速度
-        [NonSerialized] public AttackType AttackType; // 攻击类型（物理，魔法）
-        [NonSerialized] public bool CanAttack = true; // 是否可以攻击
-        [NonSerialized] public int Health; // 生命值
-        [NonSerialized] public TargetCount TargetCount; // 攻击目标数量（单体，群体，溅射）
-        [NonSerialized] public TargetMobility TargetMobility; // 攻击目标移动类型（静止敌人，移动敌人）
+        /// <summary>攻击力</summary>
+        [NonSerialized] public int Attack;
+        /// <summary>攻击冷却时间</summary>
+        [NonSerialized] public float AttackCooldown = 0f;
+        /// <summary>攻击速度</summary>
+        [NonSerialized] public float AttackSpeed = 1.0f;
+        /// <summary>攻击类型（物理，魔法）</summary>
+        [NonSerialized] public AttackType AttackType;
+        /// <summary>是否可以攻击</summary>
+        [NonSerialized] public bool CanAttack = true;
+        /// <summary>生命值</summary>
+        [NonSerialized] public int Health;
+        /// <summary>攻击目标数量（单体，群体，溅射）</summary>
+        [NonSerialized] public TargetCount TargetCount;
+        /// <summary>攻击目标移动类型（静止敌人，移动敌人）</summary>
+        [NonSerialized] public TargetMobility TargetMobility;
 
         public int SplashRadius
         {
